063776994f4d7682450ae3a82c602d2fbff156ef
Marcelo Rocha <phd.mrocha@gmail.com>
2025-09-15 20:15:10 -0300

    Update.

--- teste1.py
+++ teste1.py
@@ -1,16 +1,16 @@
 from script_engine import ScriptEngine
 
-sp1 = ScriptEngine()
+sp1 = ScriptEngine() # Empty state.
 
-if not (sp1.load_script("eva_scripts/tabuada_nova_evaml.xml")):
+if not (sp1.load_script("eva_scripts/tabuada_nova_evaml.xml")): # If file was loaded, it is in a Not_Init state.
     # We have a problem with the file.
     exit(1)
 
-sp1.initialize()
+sp1.initialize() # After initialization it is in Idle state.
 
-sp1.start_script()
+sp1.start_script() # Now, it is in Play state.
 
-while sp1.get_state() == "PLAY":
+while sp1.get_state() == "PLAY": # It is in Play until the script finish. When finished, it will be in Idle state, again.
     sp1.play_next()
 
 
